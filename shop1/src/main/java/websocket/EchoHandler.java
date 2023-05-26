f6e064085fb7a3186f8601880aa94fdf5baa3363
GD <GD@gdg>
2023-05-26 17:23:05 +0900

    20230526

--- shop1/src/main/java/websocket/EchoHandler.java
+++ shop1/src/main/java/websocket/EchoHandler.java
@@ -13,20 +13,27 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class EchoHandler extends TextWebSocketHandler implements InitializingBean{
+	//EchoHandler : 문장들만 왔다갔다 할수있음
+	//WebSocketSession : 클라이언트. 채팅 중인 한개의 브라우저
+	//현재 채팅중인 모든 브라우저의 세션 저장
 	private Set<WebSocketSession> clients = new HashSet<WebSocketSession>();
-	@Override
+	@Override //소켓 접속완료
+	//chat.jsp function 부분
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		super.afterConnectionEstablished(session);
 		System.out.println("클라이언트 접속:" + session.getId());
 		clients.add(session);
 	}
-	@Override
+	@Override //클라이언트에서 메세지 수신된 경우(데이터 주거니 받거니~)
 	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception{
+		//클라이언트가 전송한 메세지 값:  loadMessage
 		String loadMessage = (String)message.getPayload();
 		System.out.println(session.getId() + ":클라이언트 메세지:" + loadMessage);
 		clients.add(session);		
 		for(WebSocketSession s : clients) {
-			s.sendMessage(new TextMessage(loadMessage));
+			//브로드캐스팅(BroadCastting
+			s.sendMessage(new TextMessage(loadMessage)); 
+			//클라이언트에서 받은 메세지를 모든 클라이언트한테 다시 전송
 		}
 	}
 	@Override
@@ -34,13 +41,14 @@ public class EchoHandler extends TextWebSocketHandler implements InitializingBea
 		super.handleTransportError(session,exception);
 		System.out.println("오류발생 :" + exception.getMessage());
 	}
+	//브라우저와 접속 종료 (세션끊어짐)
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		super.afterConnectionClosed(session, status);
 		System.out.println("클라이언트 접속 해제 : " + status.getReason());
 		clients.remove(session);
 	}
+	//InitializingBean 의 추상메서드라 구현 필요함 없으면 에러
 	@Override
-	
 	public void afterPropertiesSet() throws Exception {}
 }
--- shop1/src/main/webapp/WEB-INF/view/chat/chat.jsp
+++ shop1/src/main/webapp/WEB-INF/view/chat/chat.jsp
@@ -2,9 +2,9 @@
     pageEncoding="UTF-8"%>
 <%@ include file="/WEB-INF/view/jspHeader.jsp" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<c:set var="port" value="${pageContext.request.localPort}" />
-<c:set var="server" value="${pageContext.request.serverName}" />
-<c:set var="path" value="${pageContext.request.contextPath}" />
+<c:set var="port" value="${pageContext.request.localPort}" /> <%-- 포트번호 : 8080 --%>
+<c:set var="server" value="${pageContext.request.serverName}" /> <%--IP 주소 : localhost --%>
+<c:set var="path" value="${pageContext.request.contextPath}" /> 
 <%--/shop1/src/main/webapp/WEB-INF/view/chat/chat.jsp --%>
 <!DOCTYPE html>
 <html>
@@ -14,19 +14,26 @@
 <script type="text/javascript">
 	$(function() {
 		let ws = new WebSocket("ws://${server}:${port}${path}/chatting")
-		ws.onopen = function() {
+		ws.onopen = function() { //서버 접속 완료인 상태
 			$("#chatStatus").text("info:connection opened")
+			//chatStatus  인 상태 화면에 출력해줌?
 			$("input[name=chatInput]").on("keydown",function(evt){
-				if(evt.keyCode == 13) {
+				if(evt.keyCode == 13) { //enter key 들어올 떄
 					let msg = $("input[name=chatInput]").val()
-					ws.send(msg)
+					//text의 value 값을 msg 에 담아서 전송
+					ws.send(msg) //서버로 데이터를 전송하자
 					$("input[name=chatInput]").val("")
 				}
 			})
 		}
+		// 서버에서 메셎지를 수신한 경우
 		ws.onmessage = function(event) {
 			$("textarea").eq(0).prepend(event.data+"\n")
+			//event.data : 수신된 메세지 정보
+			//prepend(데이터) : 앞쪽에 추가(제일 위로 올라옴)
+			//append(데이터) : 뒤쪽에 추가 (밑에 추가)
 		}
+		//서버와 접속이 종료 된 경우 발생되는 이벤트 > on 붙으면 이벤트임 
 		ws.onclose = function(event) {
 			$("#chatStatus").text("info:connection close")
 		}
